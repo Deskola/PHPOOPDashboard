6107d1a720719d9b9c6cf7990e359df483723574
Deskola <denisogunde@gmail.com>
2021-06-26 04:42:17 +0300

    MVP

--- index.php
+++ index.php
@@ -75,7 +75,9 @@
 </head>
 <body>
     <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-      <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">iPay Africa</a>
+      <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="https://ipayafrica.com" target="_blank">
+        <img src="https://www.ipayafrica.com/sites/default/files/logo-45-percent_0.svg" alt="iPay Africa" width="71" height="33">
+      </a>
       <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
